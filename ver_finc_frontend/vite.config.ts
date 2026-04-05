1089eb90733df2971155a6881b7c637e80f4a432
adailsonaguiar <adailsonacj@live.com>
2026-04-05 16:39:10 -0300

    feat: pwa

--- ver_finc_frontend/vite.config.ts
+++ ver_finc_frontend/vite.config.ts
@@ -1,9 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'maskable-icon.png'],
+      manifest: {
+        name: 'VeroFinc - Gerenciador Financeiro',
+        short_name: 'VeroFinc',
+        description: 'Seu gerenciador financeiro pessoal de alta performance.',
+        theme_color: '#4f46e5', // indigo-600
+        background_color: '#0f172a', // slate-900 (dark)
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable-icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   server: {
     port: 5173,
     proxy: {
